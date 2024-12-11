e9a03bf634d66481201fc9ed2af3b54be993719f
Diego Rubiano <diego@pigames.es>
2024-12-11 12:08:35 +0100

    wip - adding motion layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ddi.tarea6.ui"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
