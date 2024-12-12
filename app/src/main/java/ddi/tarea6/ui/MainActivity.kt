39a449279813a727d81382495a948945463f1580
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2024-12-12 14:25:58 +0100

    wip

--- app/src/main/java/ddi/tarea6/ui/MainActivity.kt
+++ app/src/main/java/ddi/tarea6/ui/MainActivity.kt
@@ -1,9 +1,18 @@
 package ddi.tarea6.ui
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuInflater
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ListPopupWindow
+import android.widget.ListView
 import android.widget.PopupMenu
+import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
@@ -21,11 +30,24 @@ class MainActivity : AppCompatActivity() {
         val buttonShowMore: MaterialButton = findViewById(R.id.button_show_more)
         buttonShowMore.setOnClickListener {
 
-            val popupMenu = PopupMenu(this, it, 0, 0, R.style.CustomPopupMenu)
+            val popupMenu = PopupMenu(this, buttonShowMore)
             popupMenu.menuInflater.inflate(R.menu.popup_menu_more, popupMenu.menu)
 
-            popupMenu.setOnMenuItemClickListener { menuItem ->
-                when (menuItem.itemId) {
+            val menuItems = mutableListOf<String>()
+            for (i in 0 until popupMenu.menu.size()) {
+                val menuItem = popupMenu.menu.getItem(i)
+                menuItems.add(menuItem.title.toString())
+            }
+            val popupView = layoutInflater.inflate(R.layout.popup_menu_layout, null)
+            val listView: ListView = popupView.findViewById(R.id.popup_list_view)
+
+            // Configura el adaptador con los títulos extraídos
+            val adapter = ArrayAdapter(this, R.layout.popup_menu_items, menuItems)
+            listView.adapter = adapter
+
+            listView.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = popupMenu.menu.getItem(position)
+                when (selectedItem.itemId) {
                     R.id.action_edit -> {
                         Toast.makeText(this, "Editar pulsado", Toast.LENGTH_SHORT).show()
                         true
@@ -34,11 +56,19 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "Ajustes pulsado", Toast.LENGTH_SHORT).show()
                         true
                     }
-                    else -> false
                 }
             }
 
-            popupMenu.show()
+            val popupWindow = PopupWindow(
+                popupView,
+                300,//LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            //popupWindow.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.popup_menu_background))
+            popupWindow.isFocusable = true
+            popupWindow.elevation = 10f
+            popupWindow.showAsDropDown(buttonShowMore, 0, 10)
+            //popupWindow.showAtLocation(buttonShowMore, Gravity.NO_GRAVITY, 600, 250)
         }
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
