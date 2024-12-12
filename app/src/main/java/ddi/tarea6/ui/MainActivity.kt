a5bc5c47846dc3f1a6d21739febbdd9850f66242
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2024-12-12 12:46:30 +0100

    wip

--- app/src/main/java/ddi/tarea6/ui/MainActivity.kt
+++ app/src/main/java/ddi/tarea6/ui/MainActivity.kt
@@ -1,13 +1,12 @@
 package ddi.tarea6.ui
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.button.MaterialButton
 import ddi.tarea6.ui.presentation.AlarmsFragment
 import ddi.tarea6.ui.presentation.StopwatchFragment
 import ddi.tarea6.ui.presentation.TimerFragment
@@ -19,11 +18,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val buttonShowMore: MaterialButton = findViewById(R.id.button_show_more)
+        buttonShowMore.setOnClickListener {
 
+            val popupMenu = PopupMenu(this, it, 0, 0, R.style.CustomPopupMenu)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu_more, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.action_edit -> {
+                        Toast.makeText(this, "Editar pulsado", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_settings -> {
+                        Toast.makeText(this, "Ajustes pulsado", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setOnItemSelectedListener { item ->
             val fragment: Fragment = when (item.itemId) {
                 R.id.nav_alarms -> AlarmsFragment()
@@ -35,26 +53,8 @@ class MainActivity : AppCompatActivity() {
             loadFragment(fragment)
             true
         }
+
         loadFragment(AlarmsFragment())
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_top_bar, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_edit -> {
-                Toast.makeText(this, "Editar pulsado", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_settings -> {
-                Toast.makeText(this, "Ajustes pulsado", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun loadFragment(fragment: Fragment) {
